0cadd3063927635a35ae32088760cab9073d9922
Lily Ma <xma50089@gmail.com>
2021-09-29 18:24:30 -0500

    Added optimizied solution for problem 1 using hashtable

--- solution1_Two_Sum.py
+++ solution1_Two_Sum.py
@@ -6,7 +6,7 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order.
 
- 
+
 Example 1:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
@@ -27,25 +27,38 @@ Constraints:
     -109 <= target <= 109
     Only one valid answer exists.
 
- 
-# Link to the solution:
+
+Link to the solution:
 https://dev.to/xshirl/two-sum-leetcode-3afp
 
 """
+
 from typing import List
 
 
-# O(n^2) time, O(1) space
-# Use two pointers(for loops) to loop through the array and find the indices of the two numbers
-# that add up to the target number.
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+    # O(n^2) time, O(1) space
+    # Use two pointers(for loops) to loop through the array and find the indices of the two numbers
+    # that add up to the target number.
+    def two_Sum_brute(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             number_to_find = target - nums[i]
             for j in range(i+1, len(nums)):
                 if nums[j] == number_to_find:
                     return [i, j]
 
+    def two_sum_hash(self, nums: List[int], target: int) -> List[int]:
+
+        nums_map = {}
+        for i in range(len(nums)):
+            number_to_find = target - nums[i]
+            if number_to_find in nums_map:
+                return [i, nums_map[number_to_find]]
+            else:
+                nums_map[nums[i]] = i
+
 
 solution = Solution()
-print(solution.twoSum([1, 3, 5, 7, 2], 9))
+print(solution.two_Sum_brute([1, 3, 5, 7, 2], 9))
+print(solution.two_sum_hash([1, 3, 5, 7, 2], 9))
